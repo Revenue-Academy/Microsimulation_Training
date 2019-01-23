a5881fdf68657bc94e0eb22af9eec69ae23bc58f
Sebastian James <sebastiansajie@yahoo.com>
2019-01-22 20:40:22 -0500

    Add files via upload

--- taxcalc/tests/conftest.py
+++ taxcalc/tests/conftest.py
@@ -19,6 +19,16 @@ def pit_path(tests_path):
 
 
 @pytest.fixture(scope='session')
+def cit_crosspath(tests_path):
+    return os.path.join(tests_path, '..', 'cit_cross.csv')
+
+
+@pytest.fixture(scope='session')
+def cit_panelpath(tests_path):
+    return os.path.join(tests_path, '..', 'cit_panel.csv')
+
+
+@pytest.fixture(scope='session')
 def pit_fullsample(pit_path):
     return pandas.read_csv(pit_path)
 
@@ -27,3 +37,13 @@ def pit_fullsample(pit_path):
 def pit_subsample(pit_fullsample):
     # TODO: when have larger pit_fullsample, reduce value of frac
     return pit_fullsample.sample(frac=0.10, random_state=123456789)
+
+
+@pytest.fixture(scope='session')
+def cit_crosssample(cit_crosspath):
+    return pandas.read_csv(cit_crosspath)
+
+
+@pytest.fixture(scope='session')
+def cit_panelsample(cit_panelpath):
+    return pandas.read_csv(cit_panelpath)
--- taxcalc/tests/test_calculator.py
+++ taxcalc/tests/test_calculator.py
@@ -9,30 +9,35 @@ import copy
 import pytest
 import numpy as np
 import pandas as pd
-from taxcalc import Policy, Records, Calculator
+from taxcalc import Policy, Records, CorpRecords, Calculator
 
 
-def test_incorrect_Calculator_instantiation(pit_subsample):
+def test_incorrect_Calculator_instantiation(pit_subsample, cit_crosssample):
     pol = Policy()
     rec = Records(data=pit_subsample)
+    crec = CorpRecords(data=cit_crosssample)
     with pytest.raises(ValueError):
-        Calculator(policy=None, records=rec)
+        Calculator(policy=None, records=rec, corprecords=crec)
     with pytest.raises(ValueError):
-        Calculator(policy=pol, records=None)
+        Calculator(policy=pol, records=None, corprecords=crec)
     with pytest.raises(ValueError):
         Policy(num_years=0)
 
 
-def test_correct_Calculator_instantiation(pit_fullsample, pit_subsample):
+def test_correct_Calculator_instantiation(pit_fullsample, pit_subsample,
+                                          cit_crosssample):
     syr = Policy.JSON_START_YEAR
     pol = Policy()
     assert pol.current_year == syr
     # specify expected number of filers and aggregate PIT liability
     expect_weight = 35.241e6
-    expect_pitax = 1822.119e9
+    expect_pitax = 1812.601e9
+    # expect_corpweight = ???
+    # expect_citax = ???
     # create full-sample Calculator object
     rec_full = Records(data=pit_fullsample)
-    calc_full = Calculator(policy=pol, records=rec_full)
+    crec = CorpRecords(data=cit_crosssample)
+    calc_full = Calculator(policy=pol, records=rec_full, corprecords=crec)
     assert isinstance(calc_full, Calculator)
     assert calc_full.current_year == syr
     assert calc_full.records_current_year() == syr
@@ -41,6 +46,7 @@ def test_correct_Calculator_instantiation(pit_fullsample, pit_subsample):
     actual_full_pitax = calc_full.weighted_total('pitax')
     assert np.allclose([actual_full_weight], [expect_weight])
     assert np.allclose([actual_full_pitax], [expect_pitax])
+    # TODO: test for corporate results
     # create sub-sample Calculator object
     """
     rec_sub = Records(data=pit_subsample)
@@ -51,3 +57,49 @@ def test_correct_Calculator_instantiation(pit_fullsample, pit_subsample):
     assert np.allclose([actual_sub_weight], [expect_weight])
     assert np.allclose([actual_sub_pitax], [expect_pitax], rtol=0.07)
     """
+
+
+def test_Calculator_results_consistency(pit_fullsample, cit_crosssample):
+    # generate calculated-variable dataframe for full sample in second year
+    recs = Records(data=pit_fullsample)
+    crecs = CorpRecords(data=cit_crosssample)
+    calc = Calculator(policy=Policy(), records=recs, corprecords=crecs)
+    assert isinstance(calc, Calculator)
+    assert calc.current_year == Policy.JSON_START_YEAR
+    calc.advance_to_year(Policy.JSON_START_YEAR + 1)
+    assert calc.current_year == Policy.JSON_START_YEAR + 1
+    calc.calc_all()
+    varlist = list(Records.CALCULATED_VARS)
+    vdf = calc.dataframe(varlist)
+    assert isinstance(vdf, pd.DataFrame)
+    # check consistency of calculated results individual by individual
+    assert np.allclose(vdf['TTI'],
+                       vdf['GTI'] - vdf['deductions'])
+    assert np.allclose(vdf['Aggregate_Income'],
+                       np.maximum(0., vdf['TTI'] - vdf['TI_special_rates']))
+    assert np.all(vdf['Tax_ST_CG_RATE1'] >= 0.)
+    assert np.all(vdf['Tax_ST_CG_RATE2'] >= 0.)
+    assert np.all(vdf['Tax_ST_CG_APPRATE'] == 0.)
+    assert np.allclose(vdf['Total_Tax_STCG'],
+                       (vdf['Tax_ST_CG_RATE1'] +
+                        vdf['Tax_ST_CG_RATE2'] +
+                        vdf['Tax_ST_CG_APPRATE']))
+    assert np.all(vdf['Tax_LT_CG_RATE1'] >= 0.)
+    assert np.all(vdf['Tax_LT_CG_RATE2'] >= 0.)
+    assert np.allclose(vdf['Total_Tax_LTCG'],
+                       vdf['Tax_LT_CG_RATE1'] + vdf['Tax_LT_CG_RATE2'])
+    assert np.allclose(vdf['Total_Tax_Cap_Gains'],
+                       vdf['Total_Tax_STCG'] + vdf['Total_Tax_LTCG'])
+    assert np.all(vdf['tax_Aggregate_Income'] >= 0.)
+    assert np.all(vdf['tax_TI_special_rates'] >= 0.)
+    assert np.all(vdf['rebate_agri'] >= 0.)
+    exp = vdf['tax_Aggregate_Income'] + vdf['tax_TI_special_rates']
+    exp -= vdf['rebate_agri']
+    assert np.allclose(vdf['tax_TTI'], exp)
+    assert np.all(vdf['rebate'] >= 0.)
+    assert np.all(vdf['surcharge'] >= 0.)
+    assert np.all(vdf['cess'] >= 0.)
+    assert np.all(vdf['pitax'] >= 0.)
+    exp = vdf['tax_TTI'] - vdf['rebate'] + vdf['surcharge'] + vdf['cess']
+    assert np.allclose(vdf['pitax'], exp)
+    # TODO: Add some tests for corporate results
